ef793d41ed752be308ee157a099a501c7322ea12
JackCokebb <reo2726@gmail.com>
2022-02-03 23:46:32 +0900

    file read test

--- main.js
+++ main.js
@@ -11,17 +11,26 @@ var app = http.createServer(function (request, response) {
   var queryData = url.parse(_url, true).query;
   //new way
   var title = test_url.searchParams.get("id");
-
+  let desc = "";
   //console.log(test_url);
   //console.log(queryData2);
   if (_url == "/") {
     title = "Welcome";
+    desc = "Hello! i am JEE";
   }
   if (_url == "/favicon.ico") {
     return response.writeHead(404);
   }
   response.writeHead(200);
-  let template = `
+
+  //console.log("data/" + title);
+  fs.readFile("data/" + title, "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    desc = data;
+
+    let template = `
   <!doctype html>
 <html>
 <head>
@@ -36,18 +45,15 @@ var app = http.createServer(function (request, response) {
     <li><a href="?id=JavaScript">JavaScript</a></li>
   </ol>
   <h2>${title}</h2>
-  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-  <img src="coding.jpg" width="100%">
-  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-  </p>
+  <p>${desc}</p>
 </body>
 </html>
-
   `;
-  //console.log(_url);
-  //console.log(__dirname + _url);
-  //response.end(fs.readFileSync(__dirname + _url));
-  //response.end(queryData.id);
-  response.end(template);
+    //console.log(_url);
+    //console.log(__dirname + _url);
+    //response.end(fs.readFileSync(__dirname + _url));
+    //response.end(queryData.id);
+    response.end(template);
+  });
 });
 app.listen(3000);
