f3889f79b99ea0a6281f39c21455041047e59208
JackCokebb <reo2726@gmail.com>
2022-02-09 21:35:18 +0900

    js module test

--- main.js
+++ main.js
@@ -4,33 +4,7 @@ const url = require("url");
 const qs = require("querystring");
 
 //refactoring
-const template = {
-  html: function (title, list, body, control) {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-  `;
-  },
-  list: function (files) {
-    let list = "<ul>";
-    files.forEach((elem, inx, arr) => {
-      list = list + `<li><a href="?id=${elem}">${elem}</a></li>`;
-    });
-    list = list + "</ul>";
-    return list;
-  },
-};
+const template = require("./lib/template.js");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
